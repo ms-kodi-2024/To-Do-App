f6042f00036dbd281e734049deaf8b5f342d5a27
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-01-18 20:52:03 +0100

    Add components: Card, CardForm, ColumnForm

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,22 @@
+import styles from './CardForm.module.scss';
+import { useState } from 'react';
+import TextInput from './../TextInput/TextInput';
+import Button from './../Button/Button';
+
+const CardForm = props => {
+    const [title, setTitle] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title }, props.columnId);
+        setTitle('');
+    };
+
+	return (
+        <form className={styles.cardForm} onSubmit={handleSubmit}>
+            <TextInput value={title} onChange={e => setTitle(e.target.value)} />
+            <Button>Add card</Button>
+        </form>
+	);
+};
+
+export default CardForm;
---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,27 @@
+import styles from './ColumnForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+import { useState } from 'react';
+
+const ColumnForm = props => {
+	const [title, setTitle] = useState('');
+	const [icon, setIcon] = useState('');
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		props.action({ title: title, icon: icon });
+		setTitle('');
+		setIcon('');
+	};
+	
+	return (
+		<form className={styles.columnForm} onSubmit={handleSubmit}>
+			<span className={styles.span}>Title:</span><TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+			<span className={styles.span}>Icon:</span><TextInput type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+			<Button>Add column</Button>
+		</form>
+	);
+};
+
+export default ColumnForm;
+
