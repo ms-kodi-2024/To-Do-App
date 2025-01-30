8988497540fbe0fcd5b9d2ecb3c8b9964fc2a7b5
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-01-30 11:03:30 +0100

    Add routing && add component NotFound

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Favorite from './components/Favorite/Favorite.js';
+import NotFound from './components/NotFound/NotFound.js';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
-        <About />
-        <Favorite />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       </Container>
-    </main>
+ </main>
   );
 };
 
