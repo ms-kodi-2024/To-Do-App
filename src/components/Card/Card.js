b5e037f585caf30c402872a1a9ad7d1609c48c85
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-02-04 17:16:30 +0100

    Add support for remove card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,23 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
 	const dispatch = useDispatch();
 	const handleToggleFavorite = () => {
     dispatch(toggleCardFavorite(props.id));
+	};
+	const handleRemove = () => {
+    dispatch(removeCard(props.id));
   };
 
 	return (
 		<>
 			<li className={styles.card}>{props.title}
 				<button className={styles.button} onClick={handleToggleFavorite}>
-					<span className={clsx(styles.icon, 'fa', props.isFavorite ? 'fa-star' : 'fa-star-o', props.isFavorite && styles.active)} />
+				  <span className={clsx(styles.icon, 'fa', props.isFavorite ? 'fa-star' : 'fa-star-o', props.isFavorite && styles.active)} />
+				  <span className={'fa fa-trash'} onClick={handleRemove} />
 				</button>
 			</li>
 		</>
